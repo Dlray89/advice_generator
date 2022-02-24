680bbc27be3879bfb50388000ba51d6ca5dfe646
Dlray89 <dlrayjr89@gmail.com>
2022-02-24 11:41:47 -0500

    init push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import quoteIcon from './images/quote.svg'
+import diceIcon from './images/icon-dice.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [quotes, setQuotes] = useState('')
+
+  const api_url = 'https://api.adviceslip.com/advice'
+
+  const generateQuotes = () => {
+    (async () => {
+      const getAdvice = await axios.get(api_url)
+      setQuotes(getAdvice.data.slip)
+    })()
+  }
+
+  useEffect(() => {
+    generateQuotes()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="card-container">
+        <h4 className="card-container__title">Advice #{ quotes.id}</h4>
+
+        <p className="card-container__quotes">&ldquo; {quotes.advice} &rdquo;</p>
+
+        <div className="card-container__dividers">
+          <span  /><img src={quoteIcon} alt='quotes icon' /><span/>
+        </div>
+
+        <button className='card-container__btn' onClick={generateQuotes}> <img src={diceIcon} alt='dice icon' className='card-container__btn__icon' /></button>
+
+
+      </div>
+    </>
   );
 }
 
